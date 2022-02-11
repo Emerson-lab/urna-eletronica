be737adec770662660ed53c9820d3d3bfb38b5e0
Emerson-lab <trinddade29@gmail.com>
2022-02-11 11:49:26 -0300

    componente teclado

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-<div id="app"></div>
-
+  <div id="app">
+    <div class="urna">
+      <Teclado />
+    </div>
+  </div>
 </template>
 
 <script>
-import "@/css/global.css"
+import "@/css/global.css";
+import Teclado from "@/components/Teclado.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    Teclado,
+  },
+};
 </script>
 
 <style>
 #app {
- 
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.urna {
+  width: 1000px;
+  height: 500px;
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+  border-radius: 5px;
+  display: flex;
 }
 </style>
